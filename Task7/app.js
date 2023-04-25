800ef89c654f8c2a5f8d1a08b4b206a49c5d8414
Nicat Zeynalli <nicat21308@gmail.com>
2023-04-25 16:13:12 +0400

    Task 7 Added

---
+++ Task7/app.js
@@ -0,0 +1,41 @@
+let secdiyimMusiqi = Number(
+  prompt(`Sevdiyin musiqini sec 
+1.Linkin Park- in the end
+2.Stromae - papaoutai
+3.Eminem - Superman
+4.Summertime Sadness`)
+);
+
+if (secdiyimMusiqi == 1) {
+  console.log(`
+    I tried so hard and got so far
+    But in the end it doesn't even matter
+    I had to fall to lose it all
+    But in the end it doesn't even matter`);
+} else if (secdiyimMusiqi == 2) {
+  console.log(` 
+Dites-moi d'où il vient
+Enfin je saurai où je vais
+Maman dit que lorsqu'on cherche bien
+On finit toujours par trouver`);
+} else if (secdiyimMusiqi == 3) {
+  console.log(`
+You high baby?
+Yeah
+Yeah? (Hahaha)
+Talk to me
+You want me to tell you somethin'?
+Uh-huh
+I know what you wanna hear`);
+}
+
+else if(secdiyimMusiqi==4){
+    console.log(`
+Kiss me hard before you go
+Summertime sadness
+I just wanted you to know
+That baby, you the best`)
+}
+else{
+    console.log("Ele bir mahni movcud deyildir")
+}
